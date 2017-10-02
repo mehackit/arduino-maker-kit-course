cf6f8de451afd76b1cb7c920b8c6a9b1dce5d8b8
mikkoelo <mikko@mehackit.org>
2017-10-02 13:45:01 +0300

    Add files via upload

---
+++ _2.2_Led_vilkkumaan_ohjelmoimalla.ino
@@ -0,0 +1,15 @@
+void setup() {
+ pinMode(5, OUTPUT);
+ pinMode(6, OUTPUT); //Tämä ledi liitetään mukaan myöhemmässä vaiheessa
+}
+
+void loop() {
+  digitalWrite(5, HIGH);
+  delay(1000);
+  digitalWrite(6, HIGH)
+  delay(1000);
+  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(6, LOW);
+  delay(500);     //delay -arvoja voi muokata halunsa mukaan!
+}
---
+++ _2.3_Nappi_toimintaan_ohjelmoimalla.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int buttonState = digitalRead(2);
+  Serial.println(buttonState);
+
+if (buttonState == 0){
+    digitalWrite(5, HIGH); 
+  } else { 
+    digitalWrite(5, LOW); 
+  }
+
+}
