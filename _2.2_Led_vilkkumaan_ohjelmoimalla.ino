7c82ec78f13b4d306eaa00e0c2a95af912b9afb2
mikkoelo <mikko@mehackit.org>
2017-10-16 12:43:35 +0300

    Update _2.2_Led_vilkkumaan_ohjelmoimalla.ino

--- _2.2_Led_vilkkumaan_ohjelmoimalla.ino
+++ _2.2_Led_vilkkumaan_ohjelmoimalla.ino
@@ -6,10 +6,6 @@ void setup() {
 void loop() {
   digitalWrite(5, HIGH);
   delay(1000);
-  digitalWrite(6, HIGH)
-  delay(1000);
-  digitalWrite(5, LOW);
-  delay(1000);
-  digitalWrite(6, LOW);
-  delay(500);     //delay -arvoja voi muokata halunsa mukaan!
+  digitalWrite(5, LOW)
+  delay(500); //delay -arvoja voi muokata halunsa mukaan!
 }
