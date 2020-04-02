43a0aa42050eb1d833dee58988be790217214e68
mikkoelo <mikko@mehackit.org>
2020-04-02 13:20:57 +0300

    Update 03_r2d2_musical_notes.h

--- 03_r2d2_musical_notes.h
+++ 03_r2d2_musical_notes.h
@@ -1,5 +1,6 @@
+// One version of musical note definitions for Arduino 
 // Reference:  This list was adapted from the table located here:
-// 		http://www.phy.mtu.edu/~suits/notefreqs.html
+// http://www.phy.mtu.edu/~suits/notefreqs.html
 const float  note_C0 = 16.35;  //C0
 const float  note_Db0 = 17.32;  //C#0/Db0
 const float  note_D0 = 18.35;  //D0
